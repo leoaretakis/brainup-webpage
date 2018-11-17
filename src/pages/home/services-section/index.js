baa4ebbf2b76b79aa7f586260ba3ad3cf10924da
Leonardo Aretakis <leoaretakis@gmail.com>
2018-11-17 13:49:56 +0000

    complete services section

--- src/pages/home/services-section/index.js
+++ src/pages/home/services-section/index.js
@@ -1,76 +1,167 @@
 import React from 'react'
 import QueueAnim from 'rc-queue-anim'
-import { Row, Col } from 'antd'
+import TweenOne from 'rc-tween-one'
+// import { Row, Col } from 'antd'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack'
+import Text from 'brainup-components/lib/text'
 
 import styles from './styles.module.scss'
 
-const imageSources = [
-  {
-    icon: 'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
-    title: 'Salas de estudo - 1º ao 9º anos',
-    content:
-      'Permite auxiliar os alunos na elaboração dos trabalhos de casa, na resolução de fichas formativas, no esclarecimento de dúvidas e na preparação para a realização dos testes, das provas de aferição e dos exames nacionais.'
-  },
-  {
-    icon: 'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
-    title: 'Explicações individuais - 1º ao 12º anos',
-    content:
-      'Permitem um acompanhamento mais detalhado e personalizado das disciplinas, visando o desenvolvimento de aprendizagens, a melhoria/recuperação das notas e a preparação para a realização das provas de aferição e dos exames nacionais.'
-  },
-  {
-    icon: 'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
-    title: 'Aulas de línguas para adultos',
-    content:
-      'Destinam-se a adultos e visam a aquisição e a consolidação de conhecimentos e competências linguísticas - Português, Inglês e Francês. Disponibilizamos aulas de Português a cidadãos estrangeiros.'
-  },
-  {
-    icon: 'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
-    title: 'Aulas de música - Viola/guitarra',
-    content:
-      'De forma a reforçar as aprendizagens dos alunos e a proporcionar-lhes um tempo de lazer, o centro disponibiliza um professor para aulas de música (viola/guitarra). A marcação das aulas é feita semanalmente, consoante a disponibilidade do professor.'
-  }
-]
+import studyRoomImage from '../../../resources/images/study-room.svg'
+import tutorImage from '../../../resources/images/tutoring.svg'
+import languageImage from '../../../resources/images/languages.svg'
+import guitarImage from '../../../resources/images/guitar-player.svg'
 
-class ServicesSection extends React.PureComponent {
-  render() {
-    return (
-      <div className={styles.container} {...this.props}>
-        <div className={styles.content}>
-          <div className={styles.titleWrapper}>
-            <h1>Serviços</h1>
-          </div>
-          <OverPack playScale={0.3}>
-            <QueueAnim
-              type="bottom"
-              key="block"
-              leaveReverse
-              className={styles.blockWrapper}
-              component={Row}
-            >
-              {imageSources.map((item, i) => {
-                return (
-                  <Col
-                    key={i}
-                    className={styles.block}
-                    name={`block${i}`}
-                    md={8}
-                    xs={24}
-                  >
-                    <div className={styles.icon}>
-                      <img src={item.icon} width="100%" alt="img" />
-                    </div>
-                    <h3>{item.title}</h3>
-                    <p>{item.content}</p>
-                  </Col>
-                )
-              })}
-            </QueueAnim>
-          </OverPack>
-        </div>
-      </div>
-    )
-  }
+const ServicesSection = props => {
+  return (
+    <Grid fluid {...props}>
+      <Text type="displayExtraLarge" className={styles.title}>
+        Serviços
+      </Text>
+
+      <OverPack playScale={0.3} component={Row} className={styles.serviceRow}>
+        <TweenOne
+          key="img"
+          animation={{
+            x: '-=30',
+            opacity: 0,
+            type: 'from',
+            ease: 'easeOutQuad'
+          }}
+          resetStyle
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <img className={styles.image} src={studyRoomImage} alt="img" />
+        </TweenOne>
+        <QueueAnim
+          key="text"
+          type="right"
+          leaveReverse
+          ease={['easeOutQuad', 'easeInQuad']}
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <Text key="a" type="displayMedium" className={styles.sectionTitle}>
+            Salas de estudo - 1º ao 9º anos
+          </Text>
+          <Text key="b" className={styles.sectionText}>
+            Permite auxiliar os alunos na elaboração dos trabalhos de casa, na
+            resolução de fichas formativas, no esclarecimento de dúvidas e na
+            preparação para a realização dos testes, das provas de aferição e
+            dos exames nacionais.
+          </Text>
+        </QueueAnim>
+      </OverPack>
+
+      <OverPack playScale={0.3} component={Row} className={styles.serviceRow}>
+        <QueueAnim
+          key="text2"
+          type="left"
+          leaveReverse
+          ease={['easeOutQuad', 'easeInQuad']}
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <Text key="c" type="displayMedium" className={styles.sectionTitle}>
+            Explicações individuais - 1º ao 12º anos
+          </Text>
+          <Text key="d" className={styles.sectionText}>
+            Permitem um acompanhamento mais detalhado e personalizado das
+            disciplinas, visando o desenvolvimento de aprendizagens, a
+            melhoria/recuperação das notas e a preparação para a realização das
+            provas de aferição e dos exames nacionais.
+          </Text>
+        </QueueAnim>
+        <TweenOne
+          key="img2"
+          animation={{
+            x: '=30',
+            opacity: 0,
+            type: 'from',
+            ease: 'easeOutQuad'
+          }}
+          resetStyle
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <img className={styles.image} src={tutorImage} alt="img" />
+        </TweenOne>
+      </OverPack>
+
+      <OverPack playScale={0.3} component={Row} className={styles.serviceRow}>
+        <TweenOne
+          key="img"
+          animation={{
+            x: '-=30',
+            opacity: 0,
+            type: 'from',
+            ease: 'easeOutQuad'
+          }}
+          resetStyle
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <img className={styles.image} src={languageImage} alt="img" />
+        </TweenOne>
+        <QueueAnim
+          key="text"
+          type="right"
+          leaveReverse
+          ease={['easeOutQuad', 'easeInQuad']}
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <Text key="a" type="displayMedium" className={styles.sectionTitle}>
+            Aulas de línguas para adultos
+          </Text>
+          <Text key="b" className={styles.sectionText}>
+            Destinam-se a adultos e visam a aquisição e a consolidação de
+            conhecimentos e competências linguísticas - Português, Inglês e
+            Francês. Disponibilizamos aulas de Português a cidadãos
+            estrangeiros.
+          </Text>
+        </QueueAnim>
+      </OverPack>
+
+      <OverPack playScale={0.3} component={Row} className={styles.serviceRow}>
+        <QueueAnim
+          key="text2"
+          type="left"
+          leaveReverse
+          ease={['easeOutQuad', 'easeInQuad']}
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <Text key="c" type="displayMedium" className={styles.sectionTitle}>
+            Aulas de música - Viola/guitarra
+          </Text>
+          <Text key="d" className={styles.sectionText}>
+            De forma a reforçar as aprendizagens dos alunos e a
+            proporcionar-lhes um tempo de lazer, o centro disponibiliza um
+            professor para aulas de música (viola/guitarra). A marcação das
+            aulas é feita semanalmente, consoante a disponibilidade do
+            professor.
+          </Text>
+        </QueueAnim>
+        <TweenOne
+          key="img2"
+          animation={{
+            x: '=30',
+            opacity: 0,
+            type: 'from',
+            ease: 'easeOutQuad'
+          }}
+          resetStyle
+          component={Col}
+          componentProps={{ md: 6, xs: 12 }}
+        >
+          <img className={styles.image} src={guitarImage} alt="img" />
+        </TweenOne>
+      </OverPack>
+    </Grid>
+  )
 }
 
 export default ServicesSection
